160aadcdf51cc8d33c808c2cd0de4fed1a1425a0
Cristian Rodriguez <csrodriguez@itel.edu.ar>
2025-10-15 21:20:52 -0300

    Agregar función promedio

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,5 @@
-def factorial(n):
-    if n < 0:
-        return "Error: factorial de número negativo"
-    if n == 0 or n == 1:
-        return 1
-    fact = 1
-    for i in range(2, n+1):
-        fact *= i
-    return fact
+def promedio(lista):
+    if len(lista) == 0:
+        return 0
+    return sum(lista) / len(lista)
 
